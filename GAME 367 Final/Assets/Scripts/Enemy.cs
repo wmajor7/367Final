569ef6b9a1bd95676e46855fa0a186c6eacf7413
William Major <wmajor@gmu.edu>
2022-12-06 14:17:16 -0500

    setup for bird SFX (enemy and companion)

--- GAME 367 Final/Assets/Scripts/Enemy.cs
+++ GAME 367 Final/Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Enemy : MonoBehaviour
 {
@@ -35,6 +36,13 @@ public class Enemy : MonoBehaviour
     public float knockbackDuration = 1;
 
     private float timeSinceAtk = 0f;
+
+    public AudioSource birdSFX;
+    public AudioClip flap;
+    public AudioClip caw;
+    public AudioClip death;
+    public AudioClip attack;
+    public AudioClip isHit;
 
     // Start is called before the first frame update
     void Start()
@@ -224,7 +232,7 @@ public class Enemy : MonoBehaviour
         }
     }
 
-    private void OnDestroy()
+    private void PlaySFX()
     {
 
     }
